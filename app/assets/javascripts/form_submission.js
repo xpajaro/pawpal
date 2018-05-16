043b76ef832f52417c31979fdcd9e2a8a1ac9142
Bankole Oluwole <bankolemail@gmail.com>
2018-05-15 21:08:47 -0700

    answer-stats polished with error alerts

--- app/assets/javascripts/form_submission.js
+++ app/assets/javascripts/form_submission.js
@@ -5,18 +5,32 @@ $(document).ready(function(){
     var height = ($('#ddlHeightFeet').val() * 12) + (+$('#ddlHeightInches').val());
     var action_url =  `/api/v1/stats/table?weight=${weight}&height=${height}`;
 
+    $("#dvAnswerStats").fadeOut();
+
     $.ajax(action_url)
-     .done(result =>{
-      console.log(result)
-      if (result.status === "SUCCESS"){
-        setupQuizReport(result.data);
-      }
-     })
+      .done(result =>{
+        if (result.status === "SUCCESS"){
+          setupQuizReport(result.data);
+        } else{
+          $('#quizAlert').show('fade');
+        }
+      })
+      .fail (() =>{
+          $('#quizAlert').show('fade');
+      })
   });  
+
+
+  $("#btnCloseQuizAlert").click(function() {
+    $('#quizAlert').hide('fade');
+  
+  });
 
   function setupQuizReport(stats){
     var pet = stats.preferedPet;
     if (pet){
+      $("#dvAnswerStats").fadeIn();
+
       $(".lblPet").text(pet);
       $("#imgPet").attr("src",`/assets/${pet}.jpeg`);
 
@@ -26,6 +40,8 @@ $(document).ready(function(){
 
       $("#lblConfidence").text(Math.ceil(confidence*100));
       $("#lblSimilarConfidence").text(Math.ceil(similarConfidence*100));
+    } else{
+      $('quizAlert').show('fade');
     }
   }
 });
